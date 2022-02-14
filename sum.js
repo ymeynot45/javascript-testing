4f5b8c7a39325ed76d7dcf8d75338d81dc92eeee
William Karsten <yme_ynot45@hotmail.com>
2022-02-14 15:30:59 -0600

    added test to reverse strings

--- sum.js
+++ sum.js
@@ -6,8 +6,19 @@ function cap(word) {
   return word.replace(/^\w/, (c) => c.toUpperCase());
 }
 
+function rev(revWord) {
+  return revWord.split("").reverse().join("");;
+}
 
+const Calculator = function (num1, opperator, num2) {
+  return (num1 opperator num2)
+
+
+}
+
+}
 
 
 exports.cap = cap;
 exports.sum = sum;
+exports.rev = rev;
--- sum.test.js
+++ sum.test.js
@@ -1,11 +1,18 @@
 const sum = require('./sum.js');
 const cap = require('./sum.js');
+const rev = require('./sum.js');
+const calc = require('/sum.js');
 
-test('adds 1 + 2 to equal 3', () => {
+test('Adds 1 + 2 to equal 3', () => {
   expect(sum.sum(1, 2)).toBe(3);
 });
 
-
-test('takes word and makes it Word', () => {
+test('Takes word and makes it Word', () => {
   expect(cap.cap('word')).toBe('Word');
 });
+
+test('Takes putty and reverses it to yttup', () => {
+  expect(rev.rev('putty')).toBe('yttup');
+})
+
+test('Takes ')

e5dc29b0104871546fa0d1fa3fb6418501d731b5
William Karsten <yme_ynot45@hotmail.com>
2022-02-14 15:09:10 -0600

    capitalize test works

--- sum.js
+++ sum.js
@@ -2,4 +2,12 @@ function sum(a, b) {
   return a+b;
 }
 
+function cap(word) {
+  return word.replace(/^\w/, (c) => c.toUpperCase());
+}
+
+
+
+
+exports.cap = cap;
 exports.sum = sum;
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,11 @@
 const sum = require('./sum.js');
+const cap = require('./sum.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum.sum(1, 2)).toBe(3);
+});
+
+
+test('takes word and makes it Word', () => {
+  expect(cap.cap('word')).toBe('Word');
 });
